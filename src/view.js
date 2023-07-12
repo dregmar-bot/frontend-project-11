b37a375a7bb7245e0077d3f7565bd1d9429005f9
Aleksandr Manujlo <dregmar@bk.ru>
2023-07-12 13:38:33 +0600

    add on-change and yup

---
+++ src/view.js
@@ -0,0 +1,26 @@
+import onChange from 'on-change';
+
+export default (state) => {
+  const render = (_path, value) => {
+    const input = document.querySelector('#url-input');
+    input.classList.remove('is-invalid')
+    const feedbackDiv = document.querySelector('.invalid-feedback');
+    if(feedbackDiv) {
+      input.parentNode.removeChild(feedbackDiv);
+    }
+    if (value === 'invalid') {
+      input.classList.add('is-invalid');
+      const errorDiv = document.createElement('div');
+      errorDiv.textContent = state.error.message;
+      errorDiv.classList.add('invalid-feedback');
+      input.parentNode.append(errorDiv);
+      return;
+    }
+    if (value === 'valid') {
+      const form = document.querySelector('.rss-form');
+      form.reset();
+      input.focus();
+    }
+  }
+  return onChange(state, render);
+}
