5ba812d029d22deb1529c98aa5cfb952353604bf
Aleksandr Manujlo <dregmar@bk.ru>
2023-07-12 13:38:45 +0600

    add url validation

--- src/index.js
+++ src/index.js
@@ -1,5 +1,35 @@
 import  './styles.scss';
 import  'bootstrap';
+import { string } from 'yup';
+import watch from './view.js';
 
 
-console.log("Hello Ya!");
+const state = {
+  validState: '',
+  feeds: [],
+}
+const watchedState = watch(state);
+const urlSchema = string().url().test(
+  'already not',
+  'feeds already have this RSS',
+  (value) => !state.feeds.includes(value),
+);
+
+
+
+const rssForm = document.querySelector('.rss-form');
+rssForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const { url } = Object.fromEntries(new FormData(e.target));
+  urlSchema.validate(url)
+    .then((url) => {
+      state.feeds = [...state.feeds, url];
+      state.validState = '';
+      watchedState.validState = 'valid';
+    })
+    .catch((error) => {
+      state.error = error;
+      state.validState = '';
+      watchedState.validState = 'invalid';
+    });
+})
