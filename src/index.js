aa82d3994aad8aa2c441b40e42d25fd2a03b4bc3
Aleksandr Manujlo <dregmar@bk.ru>
2023-07-12 21:29:48 +0600

    refactor index.js

--- src/index.js
+++ src/index.js
@@ -2,16 +2,28 @@ import  './styles.scss';
 import  'bootstrap';
 import { string } from 'yup';
 import watch from './view.js';
+import i18n from 'i18next';
+import ru from './locales/ru.js';
+import axios from 'axios';
+import parseRss from './parser.js';
 
+
+const i18nInstance = i18n.createInstance();
+i18nInstance.init({
+  lng: 'ru',
+  resources: {
+    ru,
+  },
+})
 
 const state = {
   validState: '',
   feeds: [],
 }
-const watchedState = watch(state);
-const urlSchema = string().url().test(
+const watchedState = watch(state, i18nInstance);
+const urlSchema = string().url('errors.notValidUrl').test(
   'already not',
-  'feeds already have this RSS',
+  'errors.alreadyHave',
   (value) => !state.feeds.includes(value),
 );
 
@@ -26,6 +38,27 @@ rssForm.addEventListener('submit', (e) => {
       state.feeds = [...state.feeds, url];
       state.validState = '';
       watchedState.validState = 'valid';
+      axios.get(`https://allorigins.hexlet.app/get?disableCache=true&url=${url}`)
+      .then((response) => {
+        const parsedRss = parseRss(response.data.contents);
+        if (parsedRss.querySelector('parsererror')){
+          watchedState.error = 'errors.parserError';
+        }
+        const items = parsedRss.querySelectorAll('item');
+        const posts = [...items].map((item) => ({
+          title: item.querySelector('title').textContent,
+          link: item.querySelector('link'),
+        }))
+        const newFeed = {
+          title: parsedRss.querySelector('title').textContent,
+          description: parsedRss.querySelector('description').textContent,
+          posts
+        }
+        watchedState.feeds = [...state.feeds, newFeed];
+      })
+      .catch((_e) => {
+        watchedState.error = 'errors.networkError';
+      })
     })
     .catch((error) => {
       state.error = error;
