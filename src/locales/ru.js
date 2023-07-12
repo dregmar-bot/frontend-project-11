f8252d0236b78e53bca3b2fde74957cc30b6a4ba
Aleksandr Manujlo <dregmar@bk.ru>
2023-07-12 21:29:08 +0600

    add locales/ru.js

---
+++ src/locales/ru.js
@@ -0,0 +1,10 @@
+export default {
+  translation: {
+    errors: {
+      notValidUrl: 'Ссылка должна быть валидным URL',
+      alreadyHave: 'Ссылка уже добавлена в агрегатор',
+      parserError: 'Произошла ошибка парсинга RSS',
+      networkError: 'Ошибка сети. Попробуйте позже',
+    }
+  }
+};
