dc9ff94d3af4f228fa14f376a42802aa9a9ac08c
Aleksandr Manujlo <dregmar@bk.ru>
2023-07-20 01:59:19 +0600

    refactor webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,42 +3,42 @@
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 export default {
-    mode: process.env.NODE_ENV || 'development',
-    entry: './src/index.js',
-    output: {
-        clean: true,
-    },
-    devServer: {
-        open: true,
-        host: 'localhost',
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'index.html',
-        }),
+  mode: process.env.NODE_ENV || 'development',
+  entry: './src/index.js',
+  output: {
+    clean: true,
+  },
+  devServer: {
+    open: true,
+    host: 'localhost',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
 
-        // Add your plugins here
-        // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+    // Add your plugins here
+    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+      },
+      { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: 'url-loader?limit=10000',
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
+        use: 'file-loader',
+      },
     ],
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-            },
-            { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
-            {
-                test: /\.scss$/,
-                use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
-            },
-            {
-                test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-                use: 'url-loader?limit=10000',
-            },
-            {
-                test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
-                use: 'file-loader',
-            },
-        ],
-    },
+  },
 };
