6cfc91df4f7de0465ce2c9c17b108e7869ad8e27
Aleksandr Manujlo <dregmar@bk.ru>
2023-08-17 19:21:06 +0600

    refactor app.js

--- src/app.js
+++ src/app.js
@@ -8,6 +8,63 @@ import watch from './view.js';
 import ru from './locales/ru.js';
 import parseRss from './parser.js';
 
+const proxify = (url) => `https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(url)}`;
+
+const updateFeeds = (state) => {
+  const promises = state.feeds.map((feed) => axios.get(proxify(feed.link))
+    .then((response) => {
+      const { posts } = parseRss(response.data.contents);
+      const currentPosts = state.posts.filter((p) => p.feedId === feed.id);
+      const currentPostsTitles = currentPosts.map((p) => p.title);
+      const newPosts = posts.filter((p) => !currentPostsTitles.includes(p.title));
+      if (newPosts.length === 0) {
+        return;
+      }
+      const setPostIds = (post) => {
+        post.feedId = feed.id;
+        post.id = _.uniqueId();
+        return post;
+      };
+      newPosts.forEach(setPostIds);
+      state.posts = [...state.posts, ...newPosts];
+    })
+    .catch((e) => {
+      console.log(e);
+    }));
+  Promise.all(promises).then(() => {
+    window.setTimeout(() => updateFeeds(state), 5000);
+  });
+};
+
+const takeFeed = (url, state) => {
+  axios.get(proxify(url))
+    .then((response) => {
+      try {
+        const { feed, posts } = parseRss(response.data.contents);
+        feed.id = _.uniqueId();
+        feed.link = url;
+        const setPostIds = (post) => {
+          post.feedId = feed.id;
+          post.id = _.uniqueId();
+          return post;
+        };
+        posts.forEach(setPostIds);
+        state.formState = 'valid';
+        state.error = null;
+        state.feeds = [...state.feeds, feed];
+        state.posts = [...state.posts, ...posts];
+      } catch (e) {
+        console.log(e);
+        state.error = e.message === 'parsing error' ? 'errors.parserError' : 'errors.undefinedError';
+        state.formState = 'invalid';
+      }
+    })
+    .catch(() => {
+      state.error = 'errors.networkError';
+      state.formState = 'invalid';
+    });
+};
+
 export default () => {
   const i18nInstance = i18n.createInstance();
   i18nInstance.init({
@@ -16,14 +73,13 @@ export default () => {
       ru,
     },
   }).then(() => {
-    const state = {
-      formState: '',
-      inputDisabled: false,
-      error: '',
+    const initialState = {
+      formState: 'empty',
+      error: null,
       feeds: [],
       posts: [],
       modal: {
-        postId: 0,
+        postId: null,
       },
       uiState: {
         viewedPosts: [],
@@ -42,97 +98,35 @@ export default () => {
       feedsDiv: document.querySelector('.feeds'),
     };
 
-    const watchedState = watch(state, i18nInstance, elements);
+    const watchedState = watch(initialState, i18nInstance, elements);
     const urlSchema = string().url('errors.notValidUrl').test(
       'already not',
       'errors.alreadyHave',
-      (value) => !state.feeds.map((feed) => feed.link).includes(value),
+      (value) => !watchedState.feeds.map((feed) => feed.link).includes(value),
     );
-
-    const updateFeeds = () => {
-      const promises = state.feeds.map((feed) => axios.get(`https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(feed.link)}`)
-        .then((response) => {
-          try {
-            const { posts } = parseRss(response.data.contents);
-            const currentPosts = state.posts.filter((p) => p.feedId === feed.id);
-            const currentPostsTitles = currentPosts.map((p) => p.title);
-            const newPosts = posts.filter((p) => !currentPostsTitles.includes(p.title));
-            if (newPosts.length === 0) {
-              return;
-            }
-            const setPostIds = (post) => {
-              post.feedId = feed.id;
-              post.id = _.uniqueId();
-              return post;
-            };
-            newPosts.forEach(setPostIds);
-            watchedState.posts = [...state.posts, ...newPosts];
-          } catch (e) {
-            state.error = e.message === 'parsing error' ? 'errors.parserError' : 'errors.undefinedError';
-            watchedState.formState = 'invalid';
-          }
-        })
-        .catch(() => {
-          state.error = 'errors.networkError';
-          watchedState.formState = 'invalid';
-        }));
-      Promise.all(promises).then(() => {
-        window.setTimeout(updateFeeds, 5000);
-      });
-    };
-
-    const takeFeed = (url) => {
-      axios.get(`https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(url)}`)
-        .then((response) => {
-          try {
-            const { feed, posts } = parseRss(response.data.contents);
-            feed.id = _.uniqueId();
-            feed.link = url;
-            const setPostIds = (post) => {
-              post.feedId = feed.id;
-              post.id = _.uniqueId();
-              return post;
-            };
-            posts.forEach(setPostIds);
-            watchedState.formState = 'valid';
-            state.error = '';
-            watchedState.feeds = [...state.feeds, feed];
-            watchedState.posts = [...state.posts, ...posts];
-            updateFeeds();
-          } catch (e) {
-            state.error = e.message === 'parsing error' ? 'errors.parserError' : 'errors.undefinedError';
-            watchedState.formState = 'invalid';
-          }
-        })
-        .catch(() => {
-          state.error = 'errors.networkError';
-          watchedState.formState = 'invalid';
-        })
-        .then(() => {
-          watchedState.inputDisabled = false;
-        });
-    };
 
     const rssForm = document.querySelector('.rss-form');
     rssForm.addEventListener('submit', (e) => {
       e.preventDefault();
-      watchedState.inputDisabled = true;
+      watchedState.formState = 'sending';
       const url = new FormData(e.target).get('url');
       urlSchema.validate(url)
         .then((link) => {
-          takeFeed(link);
+          takeFeed(link, watchedState);
         })
         .catch((error) => {
-          state.error = error.message;
+          watchedState.error = error.message;
           watchedState.formState = 'invalid';
         });
     });
+
+    updateFeeds(watchedState);
 
     const postsDiv = document.querySelector('.posts');
     postsDiv.addEventListener('click', (e) => {
       if (e.target.dataset.id) {
         const { id } = e.target.dataset;
-        state.uiState.viewedPosts = [...state.uiState.viewedPosts, id];
+        watchedState.uiState.viewedPosts = [...watchedState.uiState.viewedPosts, id];
         watchedState.modal.postId = id;
       }
     });
